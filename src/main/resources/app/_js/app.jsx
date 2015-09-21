c6e7fb5b9a329d5f5aecfc889bed77315ce0d9d3
Sébastien Prunier <sebastien.prunier@gmail.com>
2015-09-22 00:16:53 +0200

    add material-ui

--- src/main/resources/app/_js/app.jsx
+++ src/main/resources/app/_js/app.jsx
@@ -1,27 +1,60 @@
 import React from 'react'
-import { Router, Route, IndexRoute, Link } from 'react-router'
+import { Router, Route, IndexRoute, Link, History } from 'react-router'
 import createBrowserHistory from 'history/lib/createBrowserHistory'
+import mui from 'material-ui';
 
 import About from './pages/about'
 import Home from './pages/home'
 
+const AppBar = mui.AppBar;
+const LeftNav = mui.LeftNav;
+const MenuItem = mui.MenuItem;
+const FontIcon = mui.FontIcon;
+const ThemeManager = new mui.Styles.ThemeManager();
+
+let injectTapEventPlugin = require("react-tap-event-plugin");
+
+//Needed for onTouchTap
+//Can go away when react 1.0 release
+//Check this repo:
+//https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
+
+let menuItems = [
+    {route: '', text: 'Home'},
+    {route: 'about', text: 'A propos'}
+];
+
 const App = React.createClass({
+    mixins: [History],
+    getChildContext() {
+        return {
+            muiTheme: ThemeManager.getCurrentTheme()
+        };
+    },
+    toggleLeftNav(e) {
+        e.preventDefault();
+        this.refs.leftNav.toggle();
+    },
+    onLeftNavChange(e, key, payload) {
+        this.history.pushState(null, '/' + payload.route, null);
+    },
     render() {
         return (
             <div id="main">
-                <h1>
-                    <Link to="/">Nantes Quizz !</Link>
-                </h1>
-                <ul>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                </ul>
+                <AppBar title="Nantes Quizz" onLeftIconButtonTouchTap={this.toggleLeftNav} />
+
+                <LeftNav ref="leftNav" docked={false} menuItems={menuItems} onChange={this.onLeftNavChange}/>
+
                 {this.props.children}
             </div>
         )
     }
 });
+
+App.childContextTypes = {
+    muiTheme: React.PropTypes.object
+};
 
 React.render(
     (
