f0568eb29731aaad5bdf9f7dc49a896a51fc2153
Sébastien Prunier <sebastien.prunier@gmail.com>
2015-09-23 18:38:38 +0200

    clevercloud metadata

--- src/main/java/com/sebprunier/nantes_quizz/App.java
+++ src/main/java/com/sebprunier/nantes_quizz/App.java
@@ -5,12 +5,14 @@ import net.codestory.http.templating.ModelAndView;
 
 public class App {
 
+    public static final int PORT = 8080;
+
     public static void main(String[] args) {
         new WebServer()
                 .configure(routes -> {
                     routes.get("/about", (context) -> ModelAndView.of("index"));
                 })
-                .start();
+                .start(PORT);
     }
 
 }
